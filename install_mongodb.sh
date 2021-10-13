789278aed5f24b95e8a7a7b89a13c5123e47067f
Буров Юрий Владимирович <burov.iuv@nsd.ru>
2021-10-14 00:48:46 +0300

    Лечим загрузку с http пакетов mongodb

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo apt-get update
+
 apt-get install apt-transport-https
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
-sudo apt-get install -y mongodb
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 
-sudo systemctl start mongodb
-sudo systemctl enable mongodb
-sudo systemctl status mongodb
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
