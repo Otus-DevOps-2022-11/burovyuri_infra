d3a6a275511af9bca86de934bd5fe789d5b1e859
Буров Юрий Владимирович <burov.iuv@nsd.ru>
2023-02-16 11:38:27 +0300

    ДЗ по ansible 2, приложение задеплоено через несколько плейбуков, образы собраны через packer с применением ansible.

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -26,11 +26,11 @@ variable yc_version {
 }
 variable app_disk_image {
   description = "Disk image for reddit app"
-  default     = "fd8j6iqbm377ob9sdaqo"
+  default     = "fd8vg8g0l3ng3tj9r0c9"
 }
 variable db_disk_image {
   description = "Disk image for reddit db"
-  default     = "fd8aapqo998nninut4ou"
+  default     = "fd8df1n7ku9mtbulrvd0"
 }
 variable subnet_id {
   description = "Subnets for main"
